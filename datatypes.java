f45c8fb220f21447960d42884f5c348459cdb6e8
vharshita004 <harshita.cs231096@global.org.in>
2025-04-08 11:12:54 +0530

    datatypes

--- datatypes.java
+++ datatypes.java
@@ -1,9 +1,25 @@
-class A{
+class datatypes{
     public static void main(String[] args){
        byte a =34;
        System.out.println(a);
 
-       int b = 123;
+       short b = 45;
        System.out.println(b);
+
+       int c = 123;
+       System.out.println(c);
+
+       float d = 34.8F;
+       System.out.println(d);
+
+       double e = 4567899.6;
+       System.out.println(e);
+
+       char f = 'c';
+       System.out.println(f);
+
+       boolean g = true;
+       System.out.println(g);
     }
+
 }
