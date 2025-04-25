6997785bff6da7c027853549b9c8e3f62a37d053
vharshita004 <harshita.cs231096@global.org.in>
2025-04-25 23:49:05 +0530

    Int

---
+++ Int.java
@@ -0,0 +1,11 @@
+class Int{
+    public static void main(String[] args){
+        int a = 1_0_0_0_0;
+        System.out.println(a);
+
+        
+       //not allowed
+       // int b = _10_0;
+        
+    }
+}
