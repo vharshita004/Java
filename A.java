e220a84abd31db87780a1f284b5df10f8f3b41d5
vharshita004 <harshita.cs231096@global.org.in>
2025-04-27 23:20:48 +0530

    B.java

---
+++ A.java
@@ -0,0 +1,6 @@
+class A{
+    static float x = 98.555F;
+    public static void main(String[] args){
+        System.out.println(x);
+    }
+}
---
+++ B.java
@@ -0,0 +1,14 @@
+class B{
+    int a = 2;
+    int b = 5;
+    int add;
+    void sum(){
+       add = a+b;
+       System.out.println(add);
+     }
+       public static void main(String[] args){
+        B x = new B();
+        x.sum();
+  }
+    
+}
