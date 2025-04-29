2562d3f1a4bfe36f7fe3e73af7db08e7565ff99e
vharshita004 <harshita.cs231096@global.org.in>
2025-04-29 23:56:04 +0530

    a12

---
+++ a12.java
@@ -0,0 +1,10 @@
+class a12{
+
+public static void main(String[] args){
+    int []x = {10,20,30,40,50,60,70};
+    int sum=0;
+    for(int i=0;i<x.length;i++)
+    sum = sum + x[i];
+    System.out.print(sum);
+  }
+}
