8dee1ce26078a4e83366004c2002770a460f0b6a
vharshita004 <harshita.cs231096@global.org.in>
2025-05-03 23:44:46 +0530

    a14

---
+++ a13.java
@@ -0,0 +1,9 @@
+class a13{
+    public static void main(String[] args){
+        int x[][]=new int[4][3];
+        System.out.println(x.length);
+        System.out.println(x[0].length);
+        System.out.println(x[1].length);
+        System.out.println(x[2].length);
+    }
+}
---
+++ a14.java
@@ -0,0 +1,11 @@
+class a14{
+    public static void main(String[] args){
+        String arr[][] = new String[3][2];
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[0][2]);
+        System.out.println(arr[1][1]);
+    }
+}
