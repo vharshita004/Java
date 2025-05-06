394b7fa416963d7a5102cdb4fe7d65387f79f4f7
vharshita004 <harshita.cs231096@global.org.in>
2025-05-06 23:56:31 +0530

    a17

---
+++ a17.java
@@ -0,0 +1,10 @@
+//Short cut technique
+class a17{
+    public static void main(String[] args){
+       // String x = new String[]{};
+       String x[] = new String[]{"Bheem", "Raju", "Jaggu", "Kaliya"};
+        System.out.println(x.length);
+        for(int i=0;i<x.length;i++ )
+        System.out.println(x[i]);
+    }
+}
